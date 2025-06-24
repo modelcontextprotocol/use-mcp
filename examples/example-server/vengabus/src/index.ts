f53bc6867bc0cca06d0cbc59a5b9351062121add
Glen Maddern <glen@glenmaddern.com>
2025-06-24 17:26:16 +1000

    Added a simpe Hono server to test

--- examples/example-server/vengabus/src/index.ts
+++ examples/example-server/vengabus/src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from "hono";
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StreamableHTTPTransport } from '@hono/mcp'
+import { Hono } from 'hono'
+import { z } from "zod";
+import { cors } from 'hono/cors'
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono()
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
-});
+// Your MCP server implementation
+const mcpServer = new McpServer({
+  name: 'my-mcp-server',
+  version: '1.0.0',
+})
 
-export default app;
+mcpServer.registerTool("add",
+  {
+    title: "Addition Tool",
+    description: "Add two numbers",
+    inputSchema: { a: z.number(), b: z.number() }
+  },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+  })
+);
+
+app.all('/mcp', cors(), async (c) => {
+  const transport = new StreamableHTTPTransport()
+  await mcpServer.connect(transport)
+  return transport.handleRequest(c)
+})
+
+export default app
