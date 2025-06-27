ddfdafeaabe8d1c4d06229b663c06394b6700241
Glen Maddern <glen@glenmaddern.com>
2025-06-27 11:37:12 +1000

    opencode + qwen32b
    https://opencode.ai/s/hKK8tl5k

--- examples/chat-ui/tailwind.config.js
+++ examples/chat-ui/tailwind.config.js
@@ -2,7 +2,55 @@
 module.exports = {
   theme: {
     extend: {
-      typography: {
+typography: {
+        extend: {
+          DEFAULT: {
+            css: {
+              lineHeight: '1.5',
+              marginTop: '0.5em',
+              marginBottom: '0.5em',
+              pre: { padding: '0', filter: 'brightness(96%)', border: '0', backgroundColor: 'transparent' }
+            }
+          },
+          h1: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h2: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h3: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          ul: { css: { marginTop: '0.5em', marginBottom: '0.5em' } },
+          ol: { css: { marginTop: '0.5em', marginBottom: '0.5em' } }
+        }
+          },
+          h1: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h2: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h3: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          ul: { css: { marginTop: '0.5em', marginBottom: '0.5em' } },
+          ol: { css: { marginTop: '0.5em', marginBottom: '0.5em' } }
+        }
+          },
+          h1: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h2: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          h3: { css: { marginTop: '1em', marginBottom: '0.5em' } },
+          ul: { css: { marginTop: '0.5em', marginBottom: '0.5em' } },
+          ol: { css: { marginTop: '0.5em', marginBottom: '0.5em' } }
+        }
+          },
+          h1: { marginTop: '1em', marginBottom: '0.5em' },
+          h2: { marginTop: '1em', marginBottom: '0.5em' },
+          h3: { marginTop: '1em', marginBottom: '0.5em' },
+          ul: { marginTop: '0.5em', marginBottom: '0.5em' },
+          ol: { marginTop: '0.5em', marginBottom: '0.5em' }
+        }
+          h1: { marginTop: '1em', marginBottom: '0.5em' },
+          h2: { marginTop: '1em', marginBottom: '0.5em' },
+          h3: { marginTop: '1em', marginBottom: '0.5em' },
+          ul: { marginTop: '0.5em', marginBottom: '0.5em' },
+          ol: { marginTop: '0.5em', marginBottom: '0.5em' }
+        },
+          h1: { marginTop: '1em', marginBottom: '0.5em' },
+          h2: { marginTop: '1em', marginBottom: '0.5em' },
+          h3: { marginTop: '1em', marginBottom: '0.5em' },
+          ul: { marginTop: '0.5em', marginBottom: '0.5em' },
+          ol: { marginTop: '0.5em', marginBottom: '0.5em' }
+        }
         DEFAULT: {
           css: {
             pre: {
